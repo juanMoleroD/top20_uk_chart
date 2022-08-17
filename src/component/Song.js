59d7dbf6c8c166c9732d2dd651296bb293d0ab59
juanMoleroD <juanldimeco@gmail.com>
2022-08-17 20:09:22 +0100

    further styling, changed API call to get artist and name instead of title.

--- src/component/Song.js
+++ src/component/Song.js
@@ -5,8 +5,9 @@ const Song = ({song, id}) => {
         <div className="song">
             <img src={song["im:image"][2].label} alt="cover"/>
             <div>
-                <h2>No: {id + 1 }</h2>
-                <h4>{song.title.label} </h4>
+                <h2>No. {id + 1 }</h2>
+                <h4>{song["im:name"].label} </h4>
+                <h4>{song["im:artist"].label}</h4>
                 <audio controls>
                     <source src={song["link"][1].attributes.href} type={song.link[1].attributes.type}/>
                 </audio>
